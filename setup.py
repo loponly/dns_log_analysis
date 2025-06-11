1227744a54a60576f63e2b21b67d15a8f460a40b
enkhbat <enkhbatlop@gmail.com>
2025-06-11 16:49:29 +0800

    Add initial setup.py for dns_log_analysis package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dns_log_analysis",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+)
